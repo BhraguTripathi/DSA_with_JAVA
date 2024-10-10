7a15d61b8697378b4a635856593a31c1b4f9a4c4
BhraguTripathi <bhragu.12324742@lpu.in>
2024-10-10 08:41:23 +0530

    practice question

---
+++ array/book_allocation.java
@@ -0,0 +1,7 @@
+package array;
+import java.util.*;
+public class book_allocation {
+    public static void main(String args[]){
+        
+    }
+}
--- recursion/tiling_problem.java
+++ recursion/tiling_problem.java
@@ -3,8 +3,22 @@ import java.util.*;
 public class tiling_problem {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-
-        
+        int n=sc.nextInt();
+        System.out.println(tiling(n));
         sc.close();
+    }
+
+    public static int tiling(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+
+        int vertical=tiling(n-1);
+
+        int horizontal=tiling(n-2);
+
+        int totalWays=vertical+horizontal;
+
+        return totalWays;
     }
 }
